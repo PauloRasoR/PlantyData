85046e2c621f58c1cba97691dc5911dca80d4f33
PauloRasoR <pauloricardo_rm@hotmail.com>
2023-01-04 22:00:44 -0300

    Model - View Produto - lista.

---
+++ PlentyData/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿namespace PlentyData.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string NomeFiscal { get; set; }
+        public string Referencia { get; set; }
+        public int Ncm { get; set; }
+        public int Cest { get; set; }
+        public int Gtin { get; set; }
+        public bool Ativo { get; set; }
+
+    }
+}
